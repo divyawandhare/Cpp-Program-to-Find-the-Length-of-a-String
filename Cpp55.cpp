84841c3b0990c473723d5664fa4a3db88ed1a88d
divyawandhare <68555772+divyawandhare@users.noreply.github.com>
2022-08-02 09:17:16 +0530

    Create Cpp55.cpp

---
+++ Cpp55.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str = "C++ Programming";
+
+    // you can also use str.length()
+    cout << "String Length = " << str.size();
+
+    return 0;
+}
+
+
+Output
+
+String Length = 15
